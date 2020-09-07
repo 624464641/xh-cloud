65a1068afbf9ad416a5f6b2930a8b5ccbaf21e64
xttong <xttong@e-lansen.cn>
2020-09-07 14:21:26 +0800

    q“

--- export/src/main/java/com/xh/export/service/impl/XhExportServiceImpl.java
+++ export/src/main/java/com/xh/export/service/impl/XhExportServiceImpl.java
@@ -1,6 +1,8 @@
 package com.xh.export.service.impl;
 
 import com.xh.export.service.XhExportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,6 +11,15 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class XhExportServiceImpl implements XhExportService {
+
+
+    private final MongoTemplate mongoTemplate;
+
+    @Autowired
+    public  XhExportServiceImpl(MongoTemplate mongoTemplate){
+        this.mongoTemplate = mongoTemplate;
+    }
+
 
     @Override
     public String test() {
