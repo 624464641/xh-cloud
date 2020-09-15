4941583cea450da819fcf1a9831549954313d3e8
xttong <xttong@e-lansen.cn>
2020-09-15 20:03:45 +0800

    q“

---
+++ common/src/main/java/com/xh/common/rabbitmqs/Send.java
@@ -0,0 +1,31 @@
+package com.xh.common.rabbitmqs;
+
+import com.rabbitmq.client.ConnectionFactory;
+
+import javax.annotation.Resource;
+
+/**
+ * @author xt
+ * @date 2020/9/15
+ */
+public class Send {
+
+
+    private final static String QUEUE_NAME = "MyQueue";
+
+
+
+
+    public static void send(){
+        // 创建ConnectionFactory，并且设置一些参数，比如hostname,portNumber等等
+        ConnectionFactory factory = null;
+
+
+
+
+
+    }
+
+
+
+}
