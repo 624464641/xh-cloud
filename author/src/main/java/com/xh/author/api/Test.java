345b338175e332ac66d85bef30a9ce2044bb5bf7
xttong <xttong@e-lansen.cn>
2020-09-25 18:14:14 +0800

    q“

--- author/src/main/java/com/xh/author/api/Test.java
+++ author/src/main/java/com/xh/author/api/Test.java
@@ -1,7 +1,9 @@
 package com.xh.author.api;
 
+import com.xh.author.pojo.XhUser;
 import com.xh.author.service.user.XhUserService;
 import com.xh.common.list.ArrayList;
+import javafx.collections.transformation.SortedList;
 import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
@@ -11,8 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author xt
@@ -41,34 +45,19 @@ public class Test {
 
 
     public static void main(String[] args) {
-
-        List<String> list = new ArrayList();
-        Iterator<String> iterator = list.iterator();
-        if (iterator.hasNext()) {
-
-        }
-
-        // Integer [] sources = new Integer[100] ;
-        // Integer [] targer = new Integer[100] ;
-        // for (int i = 0; i < 100; i++) {
-        //     sources[i]  = i;
-        // }
-        // for (int i = 0; i < 50; i++) {
-        //     targer[i]  = 2*i;
-        // }
-        // int count = 0;
-        // for (int i = 0; i <sources.length ; i++) {
-        //     boolean flag = false;
-        //     for (int j = 0; j <targer.length ; j++) {
-        //         count++;
-        //         if(sources[i].equals(targer[j])){
-        //             flag = true;
-        //         }
-        //     }
-        //     if(!flag){
-        //         System.out.println(sources[i]);
-        //         System.out.println("------"+count);
-        //     }
-        // }
+        List<XhUser> list = new ArrayList();
+        XhUser user = new XhUser();
+        user.setAge(10);
+        list.add(user);
+        XhUser user4 = new XhUser();
+        user4.setAge(13);
+        list.add(user4);
+        XhUser user2 = new XhUser();
+        user2.setAge(11);
+        list.add(user2);
+        XhUser user3 = new XhUser();
+        user3.setAge(12);
+        list.add(user3);
     }
 }
+
---
+++ common/src/main/java/com/xh/common/requests/MyServlet.java
@@ -0,0 +1,16 @@
+package com.xh.common.requests;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * @author xt
+ * @date 2020/9/25
+ */
+public class MyServlet  {
+
+
+}
